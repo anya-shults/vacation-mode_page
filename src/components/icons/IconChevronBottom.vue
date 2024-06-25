eb3c43dd8d5f073585b17b5f4629e6b5f677c5d9
Anna Shults <annagalanix@gmail.com>
2024-06-25 20:09:48 +0300

    refactoring

--- src/components/icons/IconChevronBottom.vue
+++ src/components/icons/IconChevronBottom.vue
@@ -1,5 +1,11 @@
 <template>
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <g clip-path="url(#clip0_2022_78)">
       <path
         fill-rule="evenodd"
